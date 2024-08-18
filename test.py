3ab4bb3cd077e7f8e7f4e131a862618bacd7b32d
swathi gowda <swathigowda1035@gmail.com>
2024-08-18 13:29:05 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 import numpy as np
 import pandas as pd
-
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score                          
 import streamlit as st
 
